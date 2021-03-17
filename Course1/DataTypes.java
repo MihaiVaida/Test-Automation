1522de47841994c8ae74b8173f1c02e9da4763e0
Mihai Vaida <75952176+MihaiVaida@users.noreply.github.com>
2021-03-17 22:03:54 +0200

    Create DataTypes.java

---
+++ Course1/DataTypes.java
@@ -0,0 +1,49 @@
+public class DataType {
+    // psvm opens static void main(String[] args)
+    public static void main(String[] args) {
+      
+        // declare a variable
+        int x;
+
+        // int = data type
+        // x = variable name
+
+        // initialize the variable
+        x = 10;
+        x = 5;
+      
+        // print method
+        System.out.println(x);
+
+        boolean login = true;
+        System.out.println(login);
+
+        boolean monday = false;
+        boolean tuesday = true;
+        System.out.println("Is today tuesday? " + luni);
+
+        System.out.println("---------------------------------");
+        double price = 150.22;
+        System.out.println(price);
+
+        double conversion = 4.78d;
+        System.out.println(conversion);
+
+        // data type + variable name = value;
+        float number = 5.55f;
+        System.out.println(number);
+
+        //byte o = [-128, 127];
+        //short o = [-32K, 32k];
+        //int o = [-2B, 2B]
+        //long
+        //float
+        //double
+        //boolean = [false, true]
+
+        char letter = 'A';
+        String nume = "John Doe";
+        System.out.println(name);
+
+    }
+}
