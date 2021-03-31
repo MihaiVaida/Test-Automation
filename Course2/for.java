86be90a24885705a43f417741793a6f52c3aeef9
Mihai Vaida <75952176+MihaiVaida@users.noreply.github.com>
2021-03-31 21:14:28 +0300

    Update for.java

---
+++ Course2/for.java
@@ -0,0 +1,16 @@
+public class ForExample {
+    public static void main(String[] args) 
+    {
+
+        /** LOOPING STATEMENTS
+         * if / switch -> do stuff -> IF something happens / Finite no. of cases
+         * */
+      
+        int var = 10;
+//        int i;
+        for (int i = 0; i < var; i++) 
+        {
+            System.out.println( i + " test");
+        }
+    }
+}
