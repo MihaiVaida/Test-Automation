4296ba7fcbda39438cc658481560ee33bc98690d
Mihai Vaida <75952176+MihaiVaida@users.noreply.github.com>
2021-04-06 23:08:06 +0300

    Create declaringVar.java

---
+++ Course4/declaringVar.java
@@ -0,0 +1,34 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class DeclaringVariables {
+
+    /**
+     * There are different kinds of variables.
+     * */
+
+    //Variables declared in a class are called FIELDS
+    int a;
+    String nume = "";
+
+    //Variables declared in a method or block of code are called Local Variables
+    public void SumOfTwoNumbers () {
+        int b = 2;
+        int c = 3;
+        int sum = b + c;
+    }
+
+  // Variables inside a method are called Parameters
+    public static double calcAvg (int nr1, int nr2) {
+        double avg;
+        if (nr1 > nr2)
+            avg = nr1 / nr2;
+        else
+            avg = nr2 / nr1;
+        return avg;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calcAvg(5,3));
+    }
+
+}
